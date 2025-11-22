42e84065e18af163e60502e458fe510e20473dc3
mahi verma <138891560+mahi-verma23@users.noreply.github.com>
2025-11-22 14:32:12 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.companion.sahaapp',
+  appName: 'saha-your-safe-companion-main',
+  webDir: 'dist'
+};
+
+export default config;
